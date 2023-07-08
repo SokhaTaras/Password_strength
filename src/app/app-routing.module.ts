01f35f25a7500a2ec7a6c3d0974f6bb1c2b7564a
Taras <sokha2411@gmail.com>
2023-07-08 11:12:38 +0300

    routing,structure,visual

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./components/main/main.component";
+import {PasswordComponent} from "./components/password/password.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: "full",
+        redirectTo: 'password'
+      },
+      {
+        path: 'password',
+        component: PasswordComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { PasswordComponent } from './components/password/password.component';
+import { PasswordStrengthComponent } from './components/password-strength/password-strength.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    PasswordComponent,
+    PasswordStrengthComponent
   ],
   imports: [
     BrowserModule,
